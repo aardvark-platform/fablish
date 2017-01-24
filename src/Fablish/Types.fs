4b50f55f1c63579a860a1b59ae7dae74d089634d
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-01-24 10:30:46 +0100

    fixed duplicate env/sub

--- src/Fablish/Types.fs
+++ src/Fablish/Types.fs
@@ -32,67 +32,6 @@ module OnRendered =
     let ignore : 'model -> 'view -> Reaction<'msg> = fun _ _ -> { clientSide = Scripts.ignore; serverSide = fun _ -> None }
 
 
-type Sub<'msg> = 
-    | NoSub
-    | TimeSub of TimeSpan * (DateTime -> 'msg) 
-    | TwoTime of Sub<'msg> * Sub<'msg>
-
-
-type Cmd<'msg> =
-    | NoCmd
-    | Cmd of Async<'msg>
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Cmd =
-    let none = NoCmd
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Sub =
-    let none = NoSub
-    let combine l r = TwoTime(l,r)
-    let rec map (f : 'a -> 'b) (s : Sub<'a>) : Sub<'b> =
-        match s with
-            | NoSub -> NoSub
-            | TimeSub(ts,g) -> TimeSub(ts,fun t -> f (g t))
-            | TwoTime(l,r) -> TwoTime(map f l, map f r)
-    let rec extract (f : Sub<'msg>) : list<TimeSpan * (DateTime -> 'msg)> =
-        match f with
-            | NoSub -> []
-            | TimeSub(ts,g) -> [ts,g]
-            | TwoTime(l,r) -> extract l @ extract r
-    let rec aggregate xs = xs |> List.fold combine NoSub
-
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Subscriptions =
-    let none = fun _ -> Sub.none
-
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Time =
-
-    let seconds = TimeSpan.FromSeconds 1.0
-    let every (interval : TimeSpan) (f : DateTime -> 'msg) = TimeSub(interval,f)
-
-
-type Env<'msg> = { run : Cmd<'msg> -> unit }
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Env =
-    let map (f : 'b -> 'a) (a : Env<'a>) : Env<'b> =
-        let run cmd =
-            match cmd with
-                | Cmd.NoCmd -> ()
-                | Cmd.Cmd c -> 
-                    async {
-                        let! c = c
-                        return f c
-                    } |> Cmd.Cmd |> a.run
-        { run = run }
-
-type Callback<'model,'msg> = 'model -> 'msg -> 'model
-
-
 type FablishInstance<'model,'msg>(m : 'model, env : Option<Env<'msg>>, update : Env<'msg> -> 'model -> 'msg -> 'model) as this =
     let viewers = HashSet<MVar<'model>>()
     let modelSubscriptions = Dictionary<_,_>()
