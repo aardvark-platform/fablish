f8623f7eedd27adfd571ad015f289530d2a07b4d
Thomas Ortner <ortner@vrvis.at>
2017-02-14 17:43:17 +0100

    added Angularhistogram including binning and real csv data.

--- src/Examples/Experiments.fs
+++ src/Examples/Experiments.fs
@@ -71,6 +71,177 @@ module RadialDiagram =
         let count = model.values |> fst |> List.length
         let center = V2i(60,60)
         let radius = 50.0
+     
+        let circle (center : V2i) (radius : float) =
+            circle [ cx (string center.X); cy (string center.Y); r (string radius); "stroke" => "black";  "strokeDasharray" => "1,1"; "fill" => "none"; "strokeWidth" => "0.1"] []
+
+        let circles = 
+            [ for i in 5 .. 5 .. 20 do yield circle center ((float i / 20.0) * radius) ]
+
+        let segment i (k,v) =                    
+            let f = v / 20.0
+            let scale = f * radius
+            let color = if i%2 = 0 then "fill" => "#D2F4FF" else "fill" => "#76E783"            
+            let p =  (float i / (float count)) * System.Math.PI * 2.0 
+            let next =  (float (i+1) / (float count)) * System.Math.PI * 2.0
+            let x a max = sin a * (max * radius) + float center.X
+            let y a max = float center.Y - cos a * (max * radius)
+            [
+                line [ "x1" =>  ~~center.X; "y1" => ~~center.Y; "x2" => ~~(x p 1.0); "y2" => ~~(y p 1.0); "stroke" => "black"; "strokeWidth" => "0.1"; "strokeDasharray" => "1,1" ] []
+                path [ "d" => sprintf "M %d %d L %f %f A %f %f 0 0 1 %f %f L %d %d" center.X center.Y (x p f) (y p f) scale scale (x next f) (y next f) center.X center.Y; color;] []
+                path [ "d" => sprintf "M %f %f A %d %d 0 0 1 %f %f"  (x p 1.05) (y p 1.05) center.X center.Y  (x next 1.05) (y next 1.05); "stroke" => "green"; "id" => sprintf "%s" k; "visibility" => "hidden"] [ ]
+            ]
+
+        div [] [
+            text "testing animation and svg"
+            svg [ viewBox "0 0 120 120"; width "500px";  ] [
+                match model.animation with
+                    | Some (_,(current,_),_) -> yield! current |> List.mapi segment |> List.concat
+                    | _ ->  yield! model.values |> fst |> List.mapi segment |> List.concat
+                yield Svg.svgElem "text" ["font-family" => "Verdana"; "fontSize" => "3.5"; "textAnchor" => "middle" ] [
+                    for (k,v) in model.values |> fst do
+                        yield Svg.svgElem "textPath" ["startOffset" => "50%"; "xlinkHref" => sprintf "#%s" k] [text (string k)]
+                ]
+                yield! circles
+            ]
+            button [onMouseClick (fun _ -> StartTween twentytwelf)] [text "2012"]
+            button [onMouseClick (fun _ -> StartTween nineteen10)] [text "1912"]
+        ]
+
+    let subscriptions m =
+        match m.animation with
+            | Some _ ->  Time.everyMs 10.0 (fun _ -> TimeStep 10.0)
+            | _ -> Sub.none
+
+    let initial = { values = nineteen10; animation = None }
+
+    let app =  
+        {
+            initial = initial
+            update = update
+            view = view
+            subscriptions = subscriptions
+            onRendered = OnRendered.ignore
+        }
+
+module AngularHistogram =
+    
+    open Aardvark.Base
+    open Fablish
+    open Fable.Helpers.Virtualdom
+    open Fable.Helpers.Virtualdom.Html
+    open FSharp.Data
+
+    type Measures = CsvProvider<"C:\Users\ortner\Desktop\cape_desire.csv">
+
+    let capeDesire = Measures.Load("C:\Users\ortner\Desktop\cape_desire.csv")
+    
+    module Binning =
+        
+        let halfShift n data =
+            let min = data |> Seq.min
+            let max = data |> Seq.max
+            let bucketSize = (max - min) / (float n)
+
+            data |> Seq.map (fun x -> (x - bucketSize / 2.0))
+
+        let indices n data =  
+            let min = data |> Seq.min
+            let max = data |> Seq.max
+            let bucketSize = (max - min) / (float n)
+            
+            data |> Seq.map (fun x -> int ((x - min) /bucketSize))
+
+        let bin n indices = 
+            let mutable buckets = [0 .. n-1]
+            
+            for i in indices do
+                buckets <- List.updateAt i (fun x -> x + 1) buckets
+
+            buckets |> List.toSeq |> Seq.map (fun x -> float x)
+                       
+    let radToDeg r = (r * 180.0) / System.Math.PI
+    let degToRad d = (d * System.Math.PI) / 180.0
+
+    let buckets = 12
+    let keys = ["N";"30°";"60°";"E";"120°";"150°";"S";"210°";"240°";"W";"300°";"330°"]
+    
+    let angles = capeDesire.Rows 
+                    |> Seq.filter (fun x -> x.MeasurementType = "DipAndStrike") 
+                    |> Seq.map (fun x -> (x.DipAzimuth)) |> Binning.halfShift buckets
+
+    for a in angles do
+        printfn "azimuth: (%A)" a
+
+    let nineteen10' = angles |> Binning.indices buckets |> Binning.bin buckets |> Seq.mapi (fun i x -> (keys.[i],x)) |> Seq.toList
+    let nineteen10 = nineteen10', 1910    
+     
+    type Data = list<string * float> * int
+    type Model = { values : Data; animation : Option<float * Data * Data> }
+
+    // wrongly taken from: http://prcweb.co.uk/radialbarchart/
+
+//    let nineteen10 = 
+//        [
+//            "N",    2.6
+//            "30°",  3.9
+//            "60°",  5.4
+//            "E",    6.0
+//            "120°", 10.1
+//            "150°", 13.3
+//            "S",    13.2
+//            "210°", 14.1
+//            "240°", 11.8
+//            "W",    9.9
+//            "300°", 2.8
+//            "330°", 5.5
+//         ], 1910
+
+    let twentytwelf =
+        [
+            "N",    2.6
+            "30°",  6.9
+            "60°",  5.4
+            "E",    3.0
+            "120°", 11.1
+            "150°", 8.3
+            "S",    13.2
+            "210°", 14.1
+            "240°", 9.8
+            "W",    1.9
+            "300°", 2.8
+            "330°", 1.5
+        ], 2012
+
+    type Action = StartTween of Data | TimeStep of float
+
+    let lerp alpha original target =
+        List.map2 (fun (k,v) (_,v')-> (k,v + (v'-v)*alpha)) original target
+
+    let update e model msg =
+        printfn "rock it."
+        match msg with
+            | StartTween d -> { model with animation = Some (0.0,model.values,d) }
+            | TimeStep ms -> 
+                match model.animation with 
+                    | None -> model
+                    | Some (l,(current,cs),(target,ts)) -> 
+                        let step = ms * 0.01
+                        if l + step >= 1.0 then { model with values = (target,ts); animation = None }
+                        else
+                            let interp =  lerp (l+step) current target 
+                            { model with animation = Some ( l+step, (interp,cs), (target,cs) )}
+
+
+    let (=>) a b = attribute a b
+
+    let inline (~~) v = string v
+
+    let view (model : Model) : DomNode<Action> = 
+        let count = model.values |> fst |> List.length
+        let center = V2i(60,60)
+        let radius = 50.0
+        let halfOffset = System.Math.PI / float count
 
         let circle (center : V2i) (radius : float) =
             circle [ cx (string center.X); cy (string center.Y); r (string radius); "stroke" => "black";  "strokeDasharray" => "1,1"; "fill" => "none"; "strokeWidth" => "0.1"] []
@@ -81,9 +252,9 @@ module RadialDiagram =
         let segment i (k,v) =                    
             let f = v / 20.0
             let scale = f * radius
-            let color = if i%2 = 0 then "fill" => "#D2F4FF" else "fill" => "#76E783"
-            let p =  (float i / (float count)) * System.Math.PI * 2.0
-            let next =  (float (i+1) / (float count)) * System.Math.PI * 2.0
+            let color = if i%2 = 0 then "fill" => "#D2F4FF" else "fill" => "#76E783"            
+            let p =  (float i / (float count)) * System.Math.PI * 2.0 - halfOffset
+            let next =  (float (i+1) / (float count)) * System.Math.PI * 2.0 - halfOffset
             let x a max = sin a * (max * radius) + float center.X
             let y a max = float center.Y - cos a * (max * radius)
             [
